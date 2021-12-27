907d02f3340cfda2313fc3f5ef9d83921065126a
xelduin <0xxardas@gmail.com>
2021-12-27 18:32:08 +0100

    feature(minigame): attack_wall test added

--- testing/l2/minigame/01_TowerDefence_test.py
+++ testing/l2/minigame/01_TowerDefence_test.py
@@ -2,6 +2,11 @@ import pytest
 import asyncio
 
 from fixtures.account import account_factory
+from utils.string import str_to_felt
+
+def uint(a):
+    return(a, 0)
+
 
 NUM_SIGNING_ACCOUNTS = 2
 
@@ -10,6 +15,24 @@ NUM_SIGNING_ACCOUNTS = 2
 def event_loop():
     return asyncio.new_event_loop()
 
+@pytest.mark.asyncio
+@pytest.mark.parametrize('account_factory', [dict(num_signers=NUM_SIGNING_ACCOUNTS)], indirect=True)
+async def test_game_creation(game_factory):
+
+    _, accounts, signers, _, _, tower_defence, tower_defence_storage = game_factory
+
+    admin_key = signers[0]
+    admin_account = accounts[0]
+
+    await admin_key.send_transaction(
+        account=admin_account,
+        to=tower_defence.contract_address,
+        selector_name='create_game',
+        calldata=[]
+    )
+
+    execution_info = await tower_defence_storage.get_latest_game_index().call()
+    assert execution_info.result == (1,)
 
 @pytest.fixture(scope='module')
 async def game_factory(account_factory):
@@ -33,10 +56,22 @@ async def game_factory(account_factory):
         to=arbiter.contract_address,
         selector_name='set_address_of_controller',
         calldata=[controller.contract_address])
+    print(admin_account)
+    attack_token = await starknet.deploy(
+        "contracts/l2/tokens/ERC20.cairo",
+        constructor_calldata=[
+            str_to_felt("ATTACK"),
+            str_to_felt("ATTACK"),
+            admin_account.contract_address,
+        ]
+    )
 
     tower_defence = await starknet.deploy(
         source="contracts/l2/minigame/01_TowerDefence.cairo",
-        constructor_calldata=[controller.contract_address]
+        constructor_calldata=[
+            controller.contract_address,
+            attack_token.contract_address
+        ]
     )
     
     tower_defence_storage = await starknet.deploy(
@@ -50,23 +85,72 @@ async def game_factory(account_factory):
         selector_name='batch_set_controller_addresses',
         calldata=[
             tower_defence.contract_address, tower_defence_storage.contract_address])
-    return starknet, accounts, signers, arbiter, controller, tower_defence, tower_defence_storage
+    return starknet, accounts, signers, arbiter, controller, attack_token, tower_defence, tower_defence_storage
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize('account_factory', [dict(num_signers=NUM_SIGNING_ACCOUNTS)], indirect=True)
-async def test_game_creation(game_factory):
+async def test_wall_attack(game_factory):
+    temp_expiry = 1
+    attack_amount = 7
 
-    _, accounts, signers, _, _, tower_defence, tower_defence_storage = game_factory
+    _, accounts, signers, _, _, attack_token, tower_defence, tower_defence_storage = game_factory
     
     admin_key = signers[0]
     admin_account = accounts[0]
 
+    execution_info = await attack_token.balanceOf(admin_account.contract_address).call()
+    assert execution_info.result.balance[0] == 1000
+
+    # 
+
     await admin_key.send_transaction(
         account=admin_account,
         to=tower_defence.contract_address,
-        selector_name='create_game',
-        calldata=[]
+        selector_name="create_game",
+        calldata=[
+            temp_expiry
+        ]
     )
 
-    execution_info = await tower_defence_storage.get_latest_game_index().call()
-    assert execution_info.result == (1,)
+    await admin_key.send_transaction(
+        account=admin_account,
+        to=attack_token.contract_address,
+        selector_name="approve",
+        calldata=[
+            tower_defence.contract_address,
+            attack_amount,0
+        ]
+    )
+
+    execution_info = await attack_token.allowance(
+        admin_account.contract_address,
+        tower_defence.contract_address
+    ).call()
+    assert execution_info.result.remaining[0] == attack_amount
+
+    await admin_key.send_transaction(
+        account=admin_account,
+        to=tower_defence.contract_address,
+        selector_name="attack_wall",
+        calldata=[
+            1,
+            attack_amount,
+        ]
+    )
+
+    execution_info = await attack_token.allowance(
+        admin_account.contract_address,
+        tower_defence.contract_address
+    ).call()
+    assert execution_info.result.remaining[0] == 0
+
+    execution_info = await attack_token.balanceOf(
+        admin_account.contract_address
+    ).call()
+    assert execution_info.result.balance[0] == 993
+
+    execution_info = await tower_defence_storage.get_wall_health(
+        1
+    ).call()
+    assert execution_info.result.health == 93
+
