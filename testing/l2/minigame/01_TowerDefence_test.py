e10b3131d325b312b2b2b72a8b656b4bd518bad3
xelduin <0xxardas@gmail.com>
2021-12-29 21:49:43 +0100

    test(minigame): testing attack through shield

--- testing/l2/minigame/01_TowerDefence_test.py
+++ testing/l2/minigame/01_TowerDefence_test.py
@@ -15,25 +15,6 @@ NUM_SIGNING_ACCOUNTS = 2
 def event_loop():
     return asyncio.new_event_loop()
 
-@pytest.mark.asyncio
-@pytest.mark.parametrize('account_factory', [dict(num_signers=NUM_SIGNING_ACCOUNTS)], indirect=True)
-async def test_game_creation(game_factory):
-
-    _, accounts, signers, _, _, tower_defence, tower_defence_storage = game_factory
-
-    admin_key = signers[0]
-    admin_account = accounts[0]
-
-    await admin_key.send_transaction(
-        account=admin_account,
-        to=tower_defence.contract_address,
-        selector_name='create_game',
-        calldata=[]
-    )
-
-    execution_info = await tower_defence_storage.get_latest_game_index().call()
-    assert execution_info.result == (1,)
-
 @pytest.fixture(scope='module')
 async def game_factory(account_factory):
     (starknet, accounts, signers) = account_factory
@@ -57,12 +38,15 @@ async def game_factory(account_factory):
         selector_name='set_address_of_controller',
         calldata=[controller.contract_address])
     print(admin_account)
-    attack_token = await starknet.deploy(
-        "contracts/l2/tokens/ERC20.cairo",
+    elements_token = await starknet.deploy(
+        "contracts/l2/tokens/ERC1155.cairo",
         constructor_calldata=[
-            str_to_felt("ATTACK"),
-            str_to_felt("ATTACK"),
             admin_account.contract_address,
+            6,
+            1,2,3,4,5,6,
+            6,
+            1000,1000,1000,1000,1000,1000,
+            1,1,1,1,1
         ]
     )
 
@@ -70,7 +54,7 @@ async def game_factory(account_factory):
         source="contracts/l2/minigame/01_TowerDefence.cairo",
         constructor_calldata=[
             controller.contract_address,
-            attack_token.contract_address
+            elements_token.contract_address
         ]
     )
     
@@ -85,72 +69,90 @@ async def game_factory(account_factory):
         selector_name='batch_set_controller_addresses',
         calldata=[
             tower_defence.contract_address, tower_defence_storage.contract_address])
-    return starknet, accounts, signers, arbiter, controller, attack_token, tower_defence, tower_defence_storage
+    return starknet, accounts, signers, arbiter, controller, elements_token, tower_defence, tower_defence_storage
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize('account_factory', [dict(num_signers=NUM_SIGNING_ACCOUNTS)], indirect=True)
-async def test_wall_attack(game_factory):
-    temp_expiry = 1
-    attack_amount = 7
+async def test_game_creation(game_factory):
 
-    _, accounts, signers, _, _, attack_token, tower_defence, tower_defence_storage = game_factory
+    _, accounts, signers, _, _, _, tower_defence, tower_defence_storage = game_factory
     
     admin_key = signers[0]
     admin_account = accounts[0]
 
-    execution_info = await attack_token.balanceOf(admin_account.contract_address).call()
-    assert execution_info.result.balance[0] == 1000
+    await admin_key.send_transaction(
+        account=admin_account,
+        to=tower_defence.contract_address,
+        selector_name='create_game',
+        calldata=[]
+    )
 
-    # 
+    execution_info = await tower_defence_storage.get_latest_game_index().call()
+    assert execution_info.result == (1,)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('account_factory', [dict(num_signers=NUM_SIGNING_ACCOUNTS)], indirect=True)
+async def test_shield_and_attack_tower(game_factory):
+
+    _, accounts, signers, _, _, elements_token, tower_defence, tower_defence_storage = game_factory
+    
+    admin_key = signers[0]
+    admin_account = accounts[0]
 
     await admin_key.send_transaction(
         account=admin_account,
         to=tower_defence.contract_address,
         selector_name="create_game",
-        calldata=[
-            temp_expiry
-        ]
+        calldata=[]
     )
+    execution_info = await tower_defence_storage.get_latest_game_index().call()
+    assert execution_info.result == (1,)
 
     await admin_key.send_transaction(
         account=admin_account,
-        to=attack_token.contract_address,
-        selector_name="approve",
+        to=elements_token.contract_address,
+        selector_name="set_approval_for_all",
         calldata=[
             tower_defence.contract_address,
-            attack_amount,0
+            1
         ]
     )
-
-    execution_info = await attack_token.allowance(
-        admin_account.contract_address,
-        tower_defence.contract_address
-    ).call()
-    assert execution_info.result.remaining[0] == attack_amount
 
     await admin_key.send_transaction(
         account=admin_account,
         to=tower_defence.contract_address,
-        selector_name="attack_wall",
+        selector_name="increase_shield",
         calldata=[
             1,
-            attack_amount,
+            4, # Water
+            3
         ]
     )
 
-    execution_info = await attack_token.allowance(
-        admin_account.contract_address,
-        tower_defence.contract_address
-    ).call()
-    assert execution_info.result.remaining[0] == 0
+    await admin_key.send_transaction(
+        account=admin_account,
+        to=tower_defence.contract_address,
+        selector_name="attack_tower",
+        calldata=[
+            1,
+            3, # Fire
+            10
+        ]
+    )
 
-    execution_info = await attack_token.balanceOf(
-        admin_account.contract_address
-    ).call()
-    assert execution_info.result.balance[0] == 993
+    execution_info = await elements_token.balance_of(
+        admin_account.contract_address,
+        3 # Fire
+        ).call()
+    assert execution_info.result.res == 990
 
     execution_info = await tower_defence_storage.get_wall_health(
         1
     ).call()
-    assert execution_info.result.health == 93
+    assert execution_info.result.health == 9993
 
+    execution_info = await tower_defence_storage.get_shield_value(
+        1,
+        4 # Water
+    ).call()
+    assert execution_info.result.value == 0
