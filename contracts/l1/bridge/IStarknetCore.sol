375596a2b6130be43494db4e778a3332f205e5a6
Abrax <lordabrax@protonmail.com>
2022-02-18 00:59:32 +0400

    Add bridge interfaces

---
+++ contracts/l1/bridge/IStarknetCore.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.6.12;
+
+interface IStarknetCore {
+    /**
+      Sends a message to an L2 contract.
+
+      Returns the hash of the message.
+    */
+    function sendMessageToL2(
+        uint256 to_address,
+        uint256 selector,
+        uint256[] calldata payload
+    ) external returns (bytes32);
+
+    /**
+      Consumes a message that was sent from an L2 contract.
+
+      Returns the hash of the message.
+    */
+    function consumeMessageFromL2(uint256 fromAddress, uint256[] calldata payload)
+        external
+        returns (bytes32);
+}
