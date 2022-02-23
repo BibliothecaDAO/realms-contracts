0b10992c43d3d6ca4ecc0422f5ff6a3e627720a4
Michael Nolivos <michaelnolivos@gmail.com>
2022-02-23 20:15:22 +0000

    chore(desiege): remove grid movement module
    also remove test

--- testing/l2/minigame/utils/GridPosition.py
+++
@@ -1,9 +0,0 @@
-# Grid contract tests need to pack/unpack x,y into single felt
-
-def unpack_position(grid_dimension, position):
-    div = position / grid_dimension
-    rem = position % grid_dimension
-    return (div,rem)
-
-def pack_position(grid_dimension, row, col):
-    return (grid_dimension * row + col)
