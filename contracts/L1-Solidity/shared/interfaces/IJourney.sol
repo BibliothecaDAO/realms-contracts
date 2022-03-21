b4233cb6049cbed5cd4602b29167a33abdab30bb
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-03-21 07:24:06 +0000

    spring cleaning of unused files

--- contracts/L1-Solidity/shared/interfaces/IJourney.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
-
-interface IJourney {
-    function bridgeWithdraw(address _player, uint256[] memory _tokenIds)
-        external;
-}
--- contracts/L1-Solidity/shared/interfaces/LordsToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface LordsToken is IERC20 {
-    function mint(address to, uint256 amount) external;
-
-    function getAgeDistribution(uint256 _age) external view returns (uint256);
-}
