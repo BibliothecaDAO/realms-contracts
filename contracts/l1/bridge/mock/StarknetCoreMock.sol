ca6d104475f51aa66b008e12d19389badfdef580
Abrax <lordabrax@protonmail.com>
2022-03-31 23:51:08 +0200

    Add StarknetCore mock Solidity contract

---
+++ contracts/l1/bridge/mock/StarknetCoreMock.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "../IStarknetCore.sol";
+
+contract StarknetCoreMock is IStarknetCore {
+    function sendMessageToL2(
+        uint256 to_address,
+        uint256 selector,
+        uint256[] calldata payload
+    ) external returns (bytes32) {
+      return 0x3030303030303030303030303030303030303030303030303030303030303030;
+    }
+
+    /**
+      Consumes a message that was sent from an L2 contract.
+
+      Returns the hash of the message.
+    */
+    function consumeMessageFromL2(uint256 fromAddress, uint256[] calldata payload)
+        external
+        returns (bytes32) {
+          return 0x0606060606060606060606060606060606060606060606060606060606060606;
+        }
+}
