ec7b4dc9e4b71184e6a81e757427554e48ec4cc1
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-03-31 21:47:29 +0000

    fix: Lords deploy script

--- tasks/settling/deploy_lords.ts
+++ tasks/settling/deploy_lords.ts
@@ -2,6 +2,7 @@
 import { deployContract, getDeployedAddressInt, getOwnerAccountInt } from '../helpers'
 import { toFelt } from 'starknet/dist/utils/number'
 import { BigNumberish, toBN } from 'starknet/dist/utils/number'
+import { Uint256, bnToUint256 } from 'starknet/dist/utils/uint256'
 
 async function main() {
     const contractName = 'ERC20_Mintable'
@@ -11,11 +12,11 @@ async function main() {
     const name: string = toFelt("1014")
     const symbol: string = toFelt("1014")
 
-    const initial_supply = toBN(1000).toString()
+    const initial_supply: Uint256 = bnToUint256("1000000000000000000000000")
 
     // Magically deploy + write all files and stuff 
     // TODO: Add treasury Account
-    await deployContract(contractName, contractName, [name, symbol, 18, initial_supply, ownerAccount])
+    await deployContract(contractName, contractName, [name, symbol, 18, initial_supply.low.toString(), initial_supply.high.toString(), ownerAccount, ownerAccount])
 }
 
 main().then(e => console.error(e))
