8b5a4d5daf004dd850dcf9f5a3067e8a03b255ea
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-07-20 05:25:53 +0000

    optizoor of cost structs

--- realms_cli/deploy/set_costs.py
+++ realms_cli/deploy/set_costs.py
@@ -9,30 +9,29 @@ def run(nre):
     config = Config(nre.network)
 
     # --------- BUILDING COSTS ------- #
+    building_calldata = [
+        [building_id.value, building_cost.resource_count, building_cost.bits,
+         building_cost.packed_ids, building_cost.packed_amounts, building_cost.lords, "0"]
+        for building_id, building_cost in BUILDING_COSTS.items()
+    ]
+    wrapped_send(
+        network=config.nile_network,
+        signer_alias=config.ADMIN_ALIAS,
+        contract_alias="proxy_L03_Buildings",
+        function="set_building_cost",
+        arguments=building_calldata
+    )
 
-    for building_id, building_cost in BUILDING_COSTS.items():
-        print(building_id.value, building_cost.resource_count, building_cost.bits,
-              building_cost.packed_ids, building_cost.packed_amounts)
-        wrapped_send(
-            network=config.nile_network,
-            signer_alias=config.ADMIN_ALIAS,
-            contract_alias="proxy_L03_Buildings",
-            function="set_building_cost",
-            arguments=[
-                building_id.value, building_cost.resource_count, building_cost.bits, building_cost.packed_ids, building_cost.packed_amounts, building_cost.lords, 0
-            ]
-        )
-
-    # --------- TROOP COSTS ------- #
-
-    for troop_id, troop_cost in TROOP_COSTS.items():
-        print(troop_id)
-        wrapped_send(
-            network=config.nile_network,
-            signer_alias=config.ADMIN_ALIAS,
-            contract_alias="proxy_L06_Combat",
-            function="set_troop_cost",
-            arguments=[
-                troop_id.value, troop_cost.resource_count, troop_cost.bits, troop_cost.packed_ids, troop_cost.packed_amounts
-            ]
-        )
+    # --------- BUILDING COSTS ------- #
+    troop_calldata = [
+        [troop_id.value, troop_cost.resource_count, troop_cost.bits,
+            troop_cost.packed_ids, troop_cost.packed_amounts]
+        for troop_id, troop_cost in BUILDING_COSTS.items()
+    ]
+    wrapped_send(
+        network=config.nile_network,
+        signer_alias=config.ADMIN_ALIAS,
+        contract_alias="proxy_L06_Combat",
+        function="set_troop_cost",
+        arguments=troop_calldata
+    )
