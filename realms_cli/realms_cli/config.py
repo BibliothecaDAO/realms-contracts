f4000e8f62ef518183c15bab3ef25b53da8ad943
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-09-15 03:04:40 +0000

    swap account indexs so it just works

--- realms_cli/realms_cli/config.py
+++ realms_cli/realms_cli/config.py
@@ -8,6 +8,7 @@ from realms_cli.realms_cli.config import Config
 """
 from nile import deployments
 
+
 def safe_load_deployment(alias: str, network: str):
     """Safely loads address from deployments file"""
     try:
@@ -40,13 +41,13 @@ class Config:
 
         self.ADMIN_ALIAS = "STARKNET_ADMIN_PRIVATE_KEY"
         self.ADMIN_ADDRESS, _ = safe_load_deployment(
-            "account-0", self.nile_network)
+            "account-1", self.nile_network)
 
         self.INITIAL_LORDS_SUPPLY = 500000000 * (10 ** 18)
 
         self.USER_ALIAS = "STARKNET_PRIVATE_KEY"
         self.USER_ADDRESS, _ = safe_load_deployment(
-            "account-1", self.nile_network)
+            "account-0", self.nile_network)
 
         self.ARBITER_ADDRESS, _ = safe_load_deployment(
             "arbiter", self.nile_network)
