d0b5845b1193bd6bc6b0ad7e903d2107a0ac5beb
loothero <loothero@protonmail.com>
2022-09-26 05:12:11 +0000

    setup script improvements + auto-run on container launch

--- scripts/setup_cli_env.sh
+++ scripts/setup_cli_env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#This script currently has two run modes:
-## Import account from argentx
+## Run Modes ##
+# Import account from argentx
 OPTION1_IMPORT_ARGENTX_ACCOUNT=1;
-## Generate new Account
+# Generate new Account
 OPTION2_GENERATE_ACCOUNT=2;
+# Exit
+OPTION3_EXIT=3;
 
 #Example Argentx PK: 1738894601847262057947312354369785682447196114282863931776046095852523510768
 VALID_ARGENTX_PK_REGEX='^[0-9]{76}$';
@@ -56,6 +58,7 @@ mode_selection() {
         echo "Please enter setup type";
         echo "1: Import account from ArgentX (recommended)";
         echo "2: Create new account";
+        echo "3: Exit";
         read -p 'Setup Type: ' script_mode;
         case $script_mode in
             
@@ -70,10 +73,52 @@ mode_selection() {
                 generate_account;
                 break;
             ;;
+
+            # Allow user to cleanly exit script
+            $OPTION3_EXIT)
+                exit 0;
+            ;;
             *)
                 echo 'Invalid option, please enter 1 or 2' >&2;
         esac
     done
+}
+
+update_bashrc() {
+    # if starknet private key env var isn't in bashrc
+    is_starknet_pk_in_bashrc=$(grep -c "export STARKNET_PRIVATE_KEY=" ~/.bashrc);
+    if [ $is_starknet_pk_in_bashrc -eq 0 ]; then
+        # add it
+        echo "export STARKNET_PRIVATE_KEY=$STARKNET_PRIVATE_KEY" >> ~/.bashrc;
+    fi
+    
+    # if starknet network env var isn't in bashrc
+    is_starknet_network_in_bashrc=$(grep -c "export STARKNET_NETWORK=" ~/.bashrc);
+    if [ $is_starknet_network_in_bashrc -eq 0 ]; then
+        # add it
+        echo "export STARKNET_NETWORK=$STARKNET_NETWORK" >> ~/.bashrc;
+    fi
+
+    # if starknet account env var isn't in bashrc
+    is_starknet_account_in_bashrc=$(grep -c "export STARKNET_ACCOUNT_ADDRESS=" ~/.bashrc);
+    if [ $is_starknet_account_in_bashrc -eq 0 ]; then
+        # add it
+        echo "export STARKNET_ACCOUNT_ADDRESS=$STARKNET_ACCOUNT_ADDRESS" >> ~/.bashrc;
+    fi
+
+    # if starknet public key env var isn't in bashrc
+    is_starknet_pubkey_in_bashrc=$(grep -c "export STARKNET_PUBLIC_KEY=" ~/.bashrc);
+    if [ $is_starknet_pubkey_in_bashrc -eq 0 ]; then
+        # add it
+        echo "export STARKNET_PUBLIC_KEY=$STARKNET_PUBLIC_KEY" >> ~/.bashrc;
+    fi
+
+    # if cairo path env var isn't in bashrc
+    is_cairo_path_in_bashrc=$(grep -c "export CAIRO_PATH=" ~/.bashrc);
+    if [ $is_cairo_path_in_bashrc -eq 0 ]; then
+        # add it
+        echo "export CAIRO_PATH=$CAIRO_PATH" >> ~/.bashrc;
+    fi
 }
 
 # main setup code
@@ -81,15 +126,15 @@ run_setup () {
     # set CAIRO_PATH if not already set (e.g. from Dockerfile)
     export CAIRO_PATH=${CAIRO_PATH:-/loot/realms-contracts/lib/cairo_contracts/src}
     export STARKNET_NETWORK=alpha-goerli
+    export STARKNET_PRIVATE_KEY=$STARKNET_PRIVATE_KEY
     export STARKNET_PUBLIC_KEY=`python -c 'import os; from nile.signer import Signer; private_key = int(os.environ["STARKNET_PRIVATE_KEY"]); signer = Signer(private_key); print(signer.public_key)'`
     
+    # add above env vars to bashrc
+    update_bashrc
+
+    # compile contracts
     nile compile
     nile compile lib/cairo_contracts/src/openzeppelin/account/presets/Account.cairo --account_contract
-    
-cat <<EOT > realms_cli/.env.nile
-export STARKNET_PRIVATE_KEY=$STARKNET_PRIVATE_KEY
-export STARKNET_NETWORK=$STARKNET_NETWORK
-EOT
     
 cat <<EOT > goerli.deployments.txt
 0x00155e87abe207e81645c236df829448268f636d41bf5851e5e39e27af5324ed:artifacts/abis/Lords_ERC20_Mintable.json:lords
@@ -141,7 +186,6 @@ EOT
 cat <<EOT > goerli.accounts.json
 {"$STARKNET_PUBLIC_KEY": {"address": "$STARKNET_ACCOUNT_ADDRESS", "index": 0, "alias": "STARKNET_PRIVATE_KEY"}}
 EOT
-    
     pip install realms_cli/
 }
 
@@ -151,13 +195,14 @@ has_valid_env_vars() {
         
         # and env vars are valid
         if [[ $STARKNET_PRIVATE_KEY =~ $VALID_ARGENTX_PK_REGEX ]] && [[ $STARKNET_ACCOUNT_ADDRESS =~ $VALID_ARGENTX_ACCOUNT ]]; then
-            #return true
+            echo "Found starknet env vars, skipping account import/generation"
+            # return true
             true
             return
             
         else
             # output invalid env vars to user
-            echo "Found existing env vars but one or both are invalid";
+            echo "Found starknet env vars but one or both are invalid, proceeding to account import/generation";
             echo "STARKNET_PRIVATE_KEY: $STARKNET_PRIVATE_KEY";
             echo "STARKNET_ACCOUNT_ADDRESS: $STARKNET_ACCOUNT_ADDRESS";
             echo "Redirecting to setup wizard...";
@@ -167,6 +212,7 @@ has_valid_env_vars() {
         fi
         
     else #one or both of the env vars are empty
+        echo "Did not find starknet env vars, proceeding to account import/generation";
         # so return false
         false
         return
@@ -174,6 +220,9 @@ has_valid_env_vars() {
 }
 
 main() {
+
+    # source bashrc to pickup any preset starknet env vars
+    source ~/.bashrc
 
     # if system has valid env vars already set
     if has_valid_env_vars; then
